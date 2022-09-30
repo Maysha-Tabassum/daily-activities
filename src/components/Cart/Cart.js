f680f4ccfc87f7475a623ec0580c160e8c286873
Maysha-Tabassum <mayshatabassum77@gmail.com>
2022-09-30 23:42:47 +0600

    final commit

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,45 +1,73 @@
-import React, { useState } from 'react';
-import './Cart.js'
+import React, { useEffect, useState } from 'react';
+import './Cart.css'
 
 const Cart = (props) => {
-    const [value, setValue ] = useState(0);
-
     const {cart} = props;
 
     let total = 0;
     for(const activity of cart){
         total = total + activity.duration;
     }
+    
+    const getLocalValues = () =>{
+        let list = localStorage.getItem('lists');
+        // console.log(list);
+        if(list){
+            return JSON.parse(localStorage.getItem('lists'));
+        } else {
+            return [];
+        }
+    }
+    const [inputData, setInputData] = useState(0);
+    const [value, setValue ] = useState(getLocalValues());
+
+    const addItem =() =>{
+        if(!inputData){
+
+        }else{
+            setValue([...value, inputData]);
+            setInputData(0)
+        }
+    }
+
+    useEffect(()=>{
+     localStorage.setItem('lists',JSON.stringify(value))
+    },[value])
 
     const handleBreakTime = (value) =>{
          const newValue = value * 1;
          setValue(newValue);
     }
+
+    
     return (
         <div className='cart'>
             <div>
-            <h4>Maysha Tabassum</h4>
+            <h3 className='user-name'>Maysha Tabassum</h3>
             <p>Dhaka, Bangladesh</p>
             
             </div>
             <div>
                 <h5>Add A Break</h5>
-                <div className='btn'>
-                <button onClick={()=>handleBreakTime(10)}>10m</button>
-                <button onClick={()=>handleBreakTime(20)}>20m</button>
-                <button onClick={()=>handleBreakTime(30)}>30m</button>
-                <button onClick={()=>handleBreakTime(40)}>40m</button>
+                <div >
+                <button onClick={()=>handleBreakTime(10)} onClickCapture={addItem}className='btn'>10m</button>
+                <button onClick={()=>handleBreakTime(20)} onClickCapture={addItem} className='btn'>20m</button>
+                <button onClick={()=>handleBreakTime(30)} onClickCapture={addItem} className='btn'>30m</button>
+                <button onClick={()=>handleBreakTime(40)} onClickCapture={addItem} className='btn'>40m</button>
                 </div>
             </div>
-            <div className='total-time'>
-                <h5>Activity Details</h5>
-                 <div>
-                    <p>Total Task Time: {total} minutes</p>
+            <div className=''>
+                <h5 className="total-time">Activity Details</h5>
+                 <div className='task-time'>
+                    <p>Total Task Time {total} minutes</p>
+                    
+                 </div>
+                 <div className="break-time">
+                    <p>Break Time : {value} minutes</p>
                     
                  </div>
                  <div>
-                    <p>Break Time : {value}</p>
-                    
+                    <button className='complete-btn'>Activity Completed</button>
                  </div>
             </div>
         </div>
